36c38765302d1037e6db713ac765b3b8595f989d
SVT86 <seba@DESKTOP-i7>
2022-12-30 18:31:28 -0300

    practicas en python.

--- uno.py
+++ uno.py
@@ -1,1 +1,8 @@
 print("HOLA MUNDO!")
+
+for i in [1, 2, 3, 4]:
+    print(i, end=", ") # prints: 1, 2, 3, 4,
+
+for i in [1, 3, 5, 7, 9]:
+    x = i**2 - (i-1)*(i+1)
+    print(x, end=", ") # prints 1, 1, 1, 1, 1, 
